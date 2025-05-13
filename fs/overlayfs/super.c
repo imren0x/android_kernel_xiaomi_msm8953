3190891e7e70540bd787ef4dfe7a412bb065278a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-13 09:19:28 +0800

    Revert "ovl: fake_ovl: add fake erofs alias"
    This reverts commit eff9c26b9857d583be5c9c2295e141058a525cd8.

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -684,12 +684,6 @@ static int ovl_show_options(struct seq_file *m, struct dentry *dentry)
 	struct super_block *sb = dentry->d_sb;
 	struct ovl_fs *ufs = sb->s_fs_info;
 
-	// 
-	if (strcmp(sb->s_type->name, "erofs") == 0) {
-		seq_puts(m, ",user_xattr,acl,cache_strategy=readaround");
-		goto skip;
-	}
-
 	seq_show_option(m, "lowerdir", ufs->config.lowerdir);
 	if (ufs->config.upperdir) {
 		seq_show_option(m, "upperdir", ufs->config.upperdir);
@@ -700,7 +694,6 @@ static int ovl_show_options(struct seq_file *m, struct dentry *dentry)
 	if (ufs->config.override_creds != ovl_override_creds_def)
 		seq_show_option(m, "override_creds",
 				ufs->config.override_creds ? "on" : "off");
-skip:
 	return 0;
 }
 
@@ -1439,23 +1432,13 @@ static struct file_system_type ovl_fs_type = {
 };
 MODULE_ALIAS_FS("overlay");
 
-static struct file_system_type ovl_fs_type_fake = {
-	.owner		= THIS_MODULE,
-	.name		= "erofs",
-	.mount		= ovl_mount,
-	.kill_sb	= kill_anon_super,
-};
-MODULE_ALIAS_FS("erofs");
-
 static int __init ovl_init(void)
 {
-	register_filesystem(&ovl_fs_type_fake);
 	return register_filesystem(&ovl_fs_type);
 }
 
 static void __exit ovl_exit(void)
 {
-	unregister_filesystem(&ovl_fs_type_fake);
 	unregister_filesystem(&ovl_fs_type);
 }
 
